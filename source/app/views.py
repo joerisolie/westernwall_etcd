6a02a0a2420e24ebac7c296b8410a1703458a7a4
joerisolie <Dgmt88rt>
2017-04-09 23:23:28 +0200

    Added etcd migration functionality

--- source/app/views.py
+++ source/app/views.py
@@ -5,7 +5,7 @@ import etcd
 
 from app import app,forms
 from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
-from .forms import MessageForm, colors
+from .forms import MessageForm, colors, DBMigrateForm
 
 class Message(object):
 	name = ""
@@ -33,7 +33,8 @@ class Message(object):
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET','POST'])
-def index():
+@app.route('/index/<string:do_refresh>', methods=['GET','POST'])
+def index(do_refresh=False):
 	form = MessageForm()
 
 	if form.validate_on_submit():
@@ -57,9 +58,9 @@ def index():
 	except etcd.EtcdKeyNotFound:
 		flash("Something went wrong while getting the messages from etcd, is the directory initialized?", 'error')
 
-	return render_template('index.html', title='The Western Wall', messages=messages, form=form)
+	return render_template('index.html', title='The Western Wall', messages=messages, form=form, do_refresh=do_refresh)
 
-@app.route('/dbinit', methods=['GET','POST'])
+@app.route('/dbinit', methods=['GET'])
 def dbinit():
 	#Delete all keys
 	try:
@@ -83,3 +84,37 @@ def ipcheck():
 	yourip = request.remote_addr
 	ip_access_route = request.access_route
 	return render_template('ipcheck.html', title='The Western Wall - IP Check', ipinfos = retvalue, yourip=yourip, ip_access_route=ip_access_route)
+
+@app.route('/dbmigrate', methods=['GET','POST'])
+def dbmigrate():
+	form = DBMigrateForm()
+
+	if form.validate_on_submit():
+		if not form.migratedata.data:
+			flash("DB changed to %s:%s" % (form.hostname.data, form.port.data))
+			app.etcd_client = etcd.Client(host=form.hostname.data, port=int(form.port.data))
+		else:
+			#Read all messages, change DB and write them to the new db
+			messages = []
+			try:
+				qry = app.etcd_client.read('/messages/', recursive=True)
+				for x in range(0, 36):
+					x_value = next((y for y in qry.children if y.key == '/messages/' + str(x)), None)
+					q_dict = json.loads(x_value.value)
+					messages.append(
+						Message(q_dict['name'], q_dict['message'], q_dict['rating'], q_dict['voters'], q_dict['color']))
+			except etcd.EtcdKeyNotFound:
+				flash("Something went wrong while getting the messages from etcd, is the directory initialized?",
+					  'error')
+
+			app.etcd_client = etcd.Client(host=form.hostname.data, port=int(form.port.data))
+
+			for x in range(0, 36):
+				app.etcd_client.write('/messages/' + str(x), json.dumps(messages[x].asDict()))
+
+			flash("DB migrated to %s:%s" % (form.hostname.data, form.port.data))
+
+
+		app.etcd_client = etcd.Client(host=form.hostname.data,port=int(form.port.data))
+
+	return render_template('dbmigrate.html', title='The Wester Wall - DB Migrate', form=form)
